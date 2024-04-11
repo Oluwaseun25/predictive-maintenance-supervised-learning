2b6b6d19e1bb07e871a69ac1a0d4daf696cfcadb
Oluwaseun25 <Samuelawe842@gmail.com>
2024-04-11 15:04:23 +0100

    model_training.py

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -12,12 +12,12 @@ def tune_and_fit(clf, X, y, params, task):
         f2_scorer = make_scorer(fbeta_score, pos_label=1, beta=2)
         start_time = time.time()
         grid_model = GridSearchCV(clf, param_grid=params, cv=5, scoring=f2_scorer)
-        grid_model.fit(X, y['Target'])
+        grid_model.fit(X, y)
     elif task == 'multi_class':
         f2_scorer = make_scorer(fbeta_score, beta=2, average='weighted')
         start_time = time.time()
         grid_model = GridSearchCV(clf, param_grid=params, cv=5, scoring=f2_scorer)
-        grid_model.fit(X, y['Failure Type'])
+        grid_model.fit(X, y)
 
     print('Best params:', grid_model.best_params_)
     # Print training times
@@ -29,7 +29,7 @@ def tune_and_fit(clf, X, y, params, task):
 def fit_models(clf, clf_str, X_train, X_val, y_train, y_val):
     metrics = pd.DataFrame(columns=clf_str)
     for model, model_name in zip(clf, clf_str):
-        model.fit(X_train, y_train['Target'])
+        model.fit(X_train, y_train)
         y_val_pred = model.predict(X_val)
         metrics[model_name] = eval_preds(model, X_val, y_val, y_val_pred, 'binary')[1]
     return metrics
