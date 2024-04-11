3a46565abe84974fbcd7fd2007f45ba511c74c22
Oluwaseun25 <Samuelawe842@gmail.com>
2024-04-11 10:32:07 +0100

    model_evaluation.py

--- scripts/model_evaluation.py
+++ scripts/model_evaluation.py
@@ -4,3 +4,28 @@ from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_
 from sklearn.metrics import make_scorer
 
 
+def eval_preds(model, X, y_true, y_pred, task):
+    if task == 'binary':
+        y_true = y_true['Target']
+        cm = confusion_matrix(y_true, y_pred)
+        proba = model.predict_proba(X)[:,1]
+        acc = accuracy_score(y_true, y_pred)
+        auc = roc_auc_score(y_true, proba)
+        f1 = f1_score(y_true, y_pred, pos_label=1)
+        f2 = fbeta_score(y_true, y_pred, pos_label=1, beta=2)
+        recall = recall_score(y_true, y_pred)
+        precision = precision_score(y_true, y_pred)
+    elif task == 'multi_class':
+        y_true = y_true['Failure Type']
+        cm = confusion_matrix(y_true, y_pred)
+        proba = model.predict_proba(X)
+        acc = accuracy_score(y_true, y_pred)
+        auc = roc_auc_score(y_true, proba, multi_class='ovr', average='weighted')
+        f1 = f1_score(y_true, y_pred, average='weighted')
+        f2 = fbeta_score(y_true, y_pred, beta=2, average='weighted')
+        recall = recall_score(y_true, y_pred, average='weighted')
+        precision = precision_score(y_true, y_pred, average='weighted')
+    metrics = pd.Series(data={'ACC':acc, 'AUC':auc, 'F1':f1, 'F2':f2, 'Recall': recall, 'Precision': precision})
+    metrics = round(metrics,3)
+    return cm, metrics
+
