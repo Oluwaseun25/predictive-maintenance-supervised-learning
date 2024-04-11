43acd61cb35a409fb178e6887b72e2013cd1c785
Oluwaseun25 <Samuelawe842@gmail.com>
2024-04-11 10:28:52 +0100

    model_evaluation.py

---
+++ scripts/model_evaluation.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, fbeta_score
+from sklearn.metrics import make_scorer
+
+
