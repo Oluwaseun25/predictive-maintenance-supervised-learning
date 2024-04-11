940b8a525a6f7e5fb37ab5e477c6cb79d4658640
Oluwaseun25 <Samuelawe842@gmail.com>
2024-04-11 10:35:10 +0100

    model_training.py

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, roc_auc_score, f1_score, fbeta_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import make_scorer
+from model_evaluation import eval_preds
 
 
 def tune_and_fit(clf, X, y, params, task):
